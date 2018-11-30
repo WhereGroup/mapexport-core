20ea9be3e6d2591cebffd8ac00a0f1fbcf86d2ee
Stephen Brieden <stephen.brieden@wheregroup.com>
2018-11-30 14:01:57 +0100

    split function that creates pdf into template, snapshot and legendpage

--- lib/Component/PDFExporter.php
+++ lib/Component/PDFExporter.php
@@ -18,58 +18,124 @@ class PDFExporter
         $this->resourceDir = $this->container->getParameter('kernel.root_dir') . '/Resources/MapbenderPrintBundle';
 
         $this->user = $this->getUser();
+
+        $this->odgParser = new OdgParser();
     }
 
-    public function createPDFFromTemplate(MapData $data)
+    public function createPDF(MapData $data)
     {
         $templatePath = $this->resourceDir . '/templates/' . $data->getTemplate();
 
-        $odgParser = new OdgParser();
-
         //Get page configuration (Orientation, width, height)
-        $conf = $odgParser->getConf($templatePath . '.odg');
+        $conf = $this->odgParser->getConf($templatePath . '.odg');
 
         //Create PDF
         $pdf = new PDFExtensions();
-        $pdfPages = array();
 
-        //Go through pages of template and fill pdf with objects defined there
-        $legendExists = false;
-        $legendOverflow = null;
-        $templatePageNumber = $odgParser->getPageNumber($templatePath . '.odg');
-        for ($i = 1; $i <= $templatePageNumber; $i++) {
-            //Add new Page to PDF
-            $pdfPages[$i] = new PDFPage($pdf, $data, $conf, $i, $templatePath);
-            //Fill PDF Page
-            $odgParser->getElements($pdfPages[$i], $templatePath . '.odg', $i);
-            $pdfPages[$i]->makePDFPage();
-            if ($legendExists == false && $pdfPages[$i]->containsLegend()) {
-                $legendExists = true;
-            }
-            if ($pdfPages[$i]->getLegendOverflow() != null) {
-                $legendOverflow = $pdfPages[$i]->getLegendOverflow();
-            }
+        //Create pages from template
+        $legendOverflow = $this->createPDFFromTemplate2($pdf, $data, $conf, $templatePath);
+
+        //Add pages for every geometry
+        if ($data->getFromExtra('snapshots')) {
+            $this->createSnapshots($pdf, $data, $conf, $templatePath);
         }
 
         //Add new legend page if there was no legend
         //if client wants printed legend and there was no place for it OR if there is unhandled legend overflow
-        if (($data->getPrintLegend() == 1 && !$legendExists) || $legendOverflow != null) {
-            //add legend pages as long as there is unhandled legend overflow
-            do {
-                $lpconf = array('orientation' => 'Portrait', 'pageSize' => array('height' => 29.7, 'width' => 21.0));
-                $legendPage = new PDFPage($pdf, $data, $lpconf);
+        if ($data->getLegends() != null) {
+            if ($data->getPrintLegend() == 1 || $legendOverflow != null) {
 
-                $legendPage->forceLegend();
+                $this->createLegendPage($pdf, $data, $conf, $legendOverflow);
 
-                $legendPage->setLegendOverflow($legendOverflow);
-                $legendPage->makePDFPage();
-
-                $legendOverflow = $legendPage->getLegendOverflow();
-            } while ($legendOverflow != null);
+            }
         }
+        //set page number
+        //$pdf->AliasNbPages();
 
         //return $pdfPage->getPDFPage();
         return $pdf->Output();
+    }
+
+    protected function createPDFFromTemplate($pdf, MapData $data, $conf, $templatePath)
+    {
+        $pdfPages = array();
+
+        //Go through pages of template and fill pdf with objects defined there
+        $legends = $data->getLegends();
+        $legendOverflow = null;
+        $templatePageNumber = $this->odgParser->getPageNumber($templatePath . '.odg');
+        for ($i = 1; $i <= $templatePageNumber; $i++) {
+            //Add new Page to PDF
+            $pdfPages[$i] = new PDFPage($pdf, $data, $conf, $i, $templatePath);
+            //Fill PDF Page
+            $this->odgParser->getElements($pdfPages[$i], $templatePath . '.odg', $i);
+            $pdfPages[$i]->makePDFPage();
+
+            //$this->preparePageNo($pdf);
+
+            if ($pdfPages[$i]->containsLegend() ) {
+                //if the page has a place for the legend images, return the overflow
+                $legendOverflow = $pdfPages[$i]->getLegendOverflow();
+
+            }
+        }
+        return $legendOverflow;
+    }
+
+    protected function createSnapshots($pdf, MapData $data, $conf, $templatePath)
+    {
+        foreach ($data->getFeatures() as $feature) {
+            //Create new MapData to create a new page with
+            $snapshot = clone($data);
+            //Add description field
+            if (array_key_exists('MASSNAHMEVORGID', $feature)) {
+                $snapshot->addToExtra('description',
+                    'Vorgang: ' . $data->getFromExtra('vorgangId') . " Maßnahme: " . $feature['MASSNAHMEVORGID']);
+            }
+            //set $feature as only feature
+            $snapshot->setFeatures(array($feature));
+            //Set Extent and center
+            $snapshot->fitExtentToFeatures();
+
+            //add new page
+            $pdfFeaturePage = new PDFPage($pdf, $snapshot, $conf, 1, $templatePath);
+            //Fill PDF Page
+            $this->odgParser->getElements($pdfFeaturePage, $templatePath . '.odg', 1);
+            $pdfFeaturePage->makePDFPage();
+            //$this->preparePageNo($pdf);
+        }
+
+    }
+
+    protected function createLegendPage($pdf, MapData $data, $conf, $legendOverflow)
+    {
+        //add legend pages as long as there is unhandled legend overflow
+        do {
+            //$lpconf = array('orientation' => 'Portrait', 'pageSize' => array('height' => 29.7, 'width' => 21.0));
+            $legendPage = new PDFPage($pdf, $data, $conf);
+
+            $legendPage->forceLegend();
+
+            $legendPage->setLegendOverflow($legendOverflow);
+            $legendPage->makePDFPage();
+
+            $legendOverflow = $legendPage->getLegendOverflow();
+            //$this->preparePageNo($pdf);
+        } while ($legendOverflow != null);
+
+    }
+
+    protected function preparePageNo(&$pdf)
+    {
+        // add pageNumber
+        //Position 1,5 cm von unten
+        $pdf->SetY(-7);
+        //Arial kursiv 8
+        $pdf->SetFont('Arial', 'I', intval(8));
+        //Farbe
+        $pdf->SetTextColor( 0, 0, 0);
+        //Seitenzahl
+        $pdf->Cell(0, 10, 'Seite ' . $pdf->PageNo() . '/{nb}', 0, 0, 'C');
     }
 
     protected function getUser()
